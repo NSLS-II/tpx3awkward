FIX: int overflow errors, force uint64 dtype

tpx3awkward/_utils.py
@@ -163,6 +163,7 @@ def decode_message(msg, chip, heartbeat_time: np.uint64 = 0):
     ----------
         Arrays of pixel coordinates, ToT, and timestamps.
     """
+    msg, heartbeat_time = np.uint64(msg), np.uint64(heartbeat_time)    # Force types
     x, y = decode_xy(msg, chip)  # or use x1, y1 = calculateXY(msg, chip) from the Vendor's code
     # ToA is 14 bits
     ToA = (msg >> np.uint(30)) & np.uint(0x3FFF)
@@ -175,20 +176,20 @@ def decode_message(msg, chip, heartbeat_time: np.uint64 = 0):
 
     ToA_coarse = (SPIDR << np.uint(14)) | ToA
     # pixel_bits are the two highest bits of the SPIDR (i.e. the pixelbits range covers 262143 spidr cycles)
-    pixel_bits = int((ToA_coarse >> np.uint(28)) & np.uint(0x3))
+    pixel_bits = np.int8((ToA_coarse >> np.uint(28)) & np.uint(0x3))
     # heart_bits are the bits at the same positions in the heartbeat_time
-    heart_bits = int((heartbeat_time >> np.uint(28)) & np.uint(0x3))
+    heart_bits = np.int8((heartbeat_time >> np.uint(28)) & np.uint(0x3))
     # Adjust heartbeat_time based on the difference between heart_bits and pixel_bits
     diff = heart_bits - pixel_bits
     # diff +/-1 occur when pixelbits step up
     # diff +/-3 occur when spidr counter overfills
     # diff can also be 0 -- then nothing happens -- but never +/-2
-    if diff == 1 or diff == -3:
+    if (diff == 1 or diff == -3) and (heartbeat_time > np.uint(0x10000000)):
         heartbeat_time -= np.uint(0x10000000)
     elif diff == -1 or diff == 3:
         heartbeat_time += np.uint(0x10000000)
     # Construct globaltime
-    global_time = (np.uint64(heartbeat_time) & np.uint(0xFFFFC0000000)) | (ToA_coarse & np.uint(0x3FFFFFFF))
+    global_time = (heartbeat_time & np.uint(0xFFFFFFFC0000000)) | (ToA_coarse & np.uint(0x3FFFFFFF))
     # Phase correction
     phase = np.uint((x / 2) % 16) or np.uint(16)
     # Construct timestamp with phase correction
@@ -196,8 +197,7 @@ def decode_message(msg, chip, heartbeat_time: np.uint64 = 0):
 
     return x, y, ToT, ts
 
-
-# @numba.jit(nopython=True)
+@numba.jit(nopython=True)
 def _ingest_raw_data(data):
 
     chips = np.zeros_like(data, dtype=np.uint8)
@@ -208,7 +208,6 @@ def _ingest_raw_data(data):
     heartbeat_lsb = np.uint64(0)
     heartbeat_msb = np.uint64(0)
     heartbeat_time = np.uint64(0)
-    hb = np.zeros_like(data, dtype="u8")
 
     photon_count, chip_indx, msg_run_count, expected_msg_count = 0, 0, 0, 0
     for msg in data:
@@ -227,21 +226,24 @@ def _ingest_raw_data(data):
         elif matches_nibble(msg, 0xB):
             # Type 2: photon event (id'd via 0xB upper nibble)
             chips[photon_count] = chip_indx
+            # heartbeat_time = np.uint64(0)
             _x, _y, _tot, _ts = decode_message(msg, chip_indx, heartbeat_time=heartbeat_time)
             x[photon_count] = _x
             y[photon_count] = _y
             tot[photon_count] = _tot
             ts[photon_count] = _ts
             
-            if (photon_count > 0) and (_ts - ts[photon_count-1] > 2**30):
+            if (photon_count > 0) and (_ts > ts[photon_count-1]) and (_ts - ts[photon_count-1] > 2**30):
                 prev_ts = ts[:photon_count]   # This portion needs to be adjusted
                 # Find what the current timestamp would be without global heartbeat
                 _, _, _, _ts_0 = decode_message(msg, chip_indx, heartbeat_time=np.uint64(0))
                 # Check if there is a SPIDR rollover in the beginning of the file but before heartbeat was received
-                if int(max(prev_ts[:10])) - int(min(prev_ts[-10:])) > 2**32:
+                head_max = max(prev_ts[:10])
+                tail_min = min(prev_ts[-10:])
+                if (head_max > tail_min) and (head_max - tail_min > 2**32):
                     prev_ts[prev_ts < 2**33] += np.uint64(2**34)
                     _ts_0 += 2**34
-                # Ajust the existing timestamps
+                # Ajust already processed timestamps
                 ts[:photon_count] = prev_ts + (_ts - _ts_0)
             
             photon_count += 1
@@ -254,13 +256,13 @@ def _ingest_raw_data(data):
         
         elif matches_nibble(msg, 0x4):
             # Type 4: global timestap (id'd via 0x4 upper nibble)
-            subheader = (msg >> np.uint(56)) & np.uint(0x0F)
+            subheader = (msg >> np.uint(56)) & np.uint64(0x0F)
             if subheader == 0x4:
                 # timer lsb, 32 bits of time
-                heartbeat_lsb = (msg >> np.uint(16)) & np.uint(0xFFFFFFFF)
+                heartbeat_lsb = (msg >> np.uint(16)) & np.uint64(0xFFFFFFFF)
             elif subheader == 0x5:
                 # timer msb
-                time_msg = (msg >> np.uint(16)) & np.uint(0xFFFF)
+                time_msg = (msg >> np.uint(16)) & np.uint64(0xFFFF)
                 heartbeat_msb = time_msg << np.uint(32)
                 heartbeat_time = (heartbeat_msb | heartbeat_lsb) # << np.uint(4)
                 # TODO the c++ code has large jump detection, do not understand why
@@ -269,7 +271,6 @@ def _ingest_raw_data(data):
             pass
 
             msg_run_count += 1
-            hb[photon_count] = heartbeat_time
 
         elif matches_nibble(msg, 0x7):
             # Type 5: "command" data (id'd via 0x7 upper nibble)
@@ -284,9 +285,8 @@ def _ingest_raw_data(data):
     indx = np.argsort(ts[:photon_count], kind="mergesort")
     chips = chips[indx]
     x, y, tot, ts = x[indx], y[indx], tot[indx], ts[indx]
-    hb = hb[indx]
 
-    return x, y, tot, ts, chips, hb
+    return x, y, tot, ts, chips
 
 
 def ingest_from_files(fpaths: List[Union[str, Path]]) -> Iterable[Dict[str, NDArray]]:
@@ -303,12 +303,12 @@ def ingest_from_files(fpaths: List[Union[str, Path]]) -> Iterable[Dict[str, NDAr
 
     for fpath in fpaths:
         data = raw_as_numpy(fpath)
-        x, y, tot, ts, chips, hb = _ingest_raw_data(data)
+        x, y, tot, ts, chips = _ingest_raw_data(data)
         yield {
             k.strip(): v
             for k, v in zip(
-                "x, y, tot, timestamp, chips, hb".split(","),
-                (x, y, tot, ts, chips, hb),
+                "x, y, tot, timestamp, chips".split(","),
+                (x, y, tot, ts, chips),
             )
         }
 
@@ -329,7 +329,7 @@ def ingest_raw_data(data: IA) -> Dict[str, NDArray]:
     """
     return {
         k.strip(): v
-        for k, v in zip("x, y, ToT, ts, chip_number, hb".split(","), _ingest_raw_data(data))
+        for k, v in zip("x, y, ToT, ts, chip_number".split(","), _ingest_raw_data(data))
     }
 
 # ^-- tom wrote