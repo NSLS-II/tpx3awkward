0b2d11b2cb6aac06eb5dcfc85ab2a8cc49256ac1
Joseph Hanrahan <joseph.hanrahan@stonybrook.edu>
2026-03-30 11:31:14 -0400

    ENH: expose find_umatched_tpx3_files to top-level API

--- src/tpx3awkward/processing/__init__.py
+++ src/tpx3awkward/processing/__init__.py
@@ -1,5 +1,13 @@
 from .cluster import cluster_raw_df
 from .decoding import tpx_to_raw_df
+from .files import find_unmatched_tpx3_files
 from .pipeline import convert_tpx3_file, convert_tpx3_files, convert_tpx3_files_parallel
 
-__all__ = ["cluster_raw_df", "convert_tpx3_file", "convert_tpx3_files", "convert_tpx3_files_parallel", "tpx_to_raw_df"]
+__all__ = [
+    "cluster_raw_df",
+    "convert_tpx3_file",
+    "convert_tpx3_files",
+    "convert_tpx3_files_parallel",
+    "find_unmatched_tpx3_files",
+    "tpx_to_raw_df",
+]
